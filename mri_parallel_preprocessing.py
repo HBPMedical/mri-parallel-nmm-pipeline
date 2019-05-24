4ab4d94cffc00441a1d186e051e4a7a9fbf7c944
Barinjaka Rakotomiaramanana <mirco.nasuti@chuv.ch>
2019-05-24 15:11:28 +0200

    several changes

--- mri_parallel_preprocessing.py
+++ mri_parallel_preprocessing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import argparse
 import os
 import multiprocessing
@@ -13,6 +14,8 @@ NMP_PATH = '/home/barinjaka/mri-preprocessing-pipeline'  # TODO: get rid of this
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
     argparser = argparse.ArgumentParser(description='Run SPM12 neuromorphometric pipeline')
     argparser.add_argument('input_folder')
     argparser.add_argument('output_folder')
@@ -22,25 +25,40 @@ def main():
     subjects = gen_subjects_list(args.input_folder)
     pool = multiprocessing.Pool(min(4, multiprocessing.cpu_count()))  # Temporarily limit to 4
     cmd_list = prepare_cmd_list(subjects, args.input_folder, args.output_folder, args.proto_def_file)
-    print(pool.map(run_matlab_cmd, cmd_list))
+    pool.map(run_matlab_cmd, cmd_list)
+    logging.info("Completed")
 
 
 def gen_subjects_list(root_folder):
     return os.listdir(root_folder)
 
 
-def prepare_cmd_list(subjects, input_folder, output_folder, proto_def_file, output_format=OUTPUT_FORMAT, nmp_template=NMP_TEMPLATE):
+def prepare_cmd_list(
+        subjects, input_folder, output_folder, proto_def_file, output_format=OUTPUT_FORMAT, nmp_template=NMP_TEMPLATE):
     pre = "NeuroMorphometric_pipeline('"
-    post = "','" + input_folder + "','"+output_folder+"','','"+proto_def_file+"','"+output_format+"','"+nmp_template+"')"
+    post = "','" + input_folder \
+           + "','" + output_folder\
+           + "','','" \
+           + proto_def_file \
+           + "','" \
+           + output_format \
+           + "','" \
+           + nmp_template + "')"
     return [pre + s + post for s in subjects]
 
 
 def run_matlab_cmd(matlab_cmd):
-    print "Running : " + matlab_cmd
-    eng = matlab.engine.start_matlab()
-    eng.eval("addpath(genpath('"+SPM12_PATH+"'))")
-    eng.eval("addpath(genpath('"+NMP_PATH+"'))")
-    return eng.eval(matlab_cmd)
+    logging.info("Running : " + matlab_cmd)
+    try:
+        eng = matlab.engine.start_matlab()
+        eng.eval("addpath(genpath('"+SPM12_PATH+"'))")
+        eng.eval("addpath(genpath('"+NMP_PATH+"'))")
+        ret = eng.eval(matlab_cmd)
+        logging.info("Successfully ran: " + matlab_cmd)
+    except matlab.engine.MatlabExecutionError as e:
+        logging.warning("Failed running " + matlab_cmd + ": " + e)
+        ret = None
+    return ret
 
 
 if __name__ == "__main__":
